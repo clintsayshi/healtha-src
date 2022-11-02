90f586c60e5741809b6f89e2b985ebdcd80f212c
Clinton <manakanejustin@gmail.com>
2022-11-02 15:32:57 +0200

    initial files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    container: false,
+    extend: {},
+  },
+  variants: { extend: {} },
+  plugins: [],
+};
