753e242737e653f3fa8b02719e0f4d125e621bb0
Clinton <manakanejustin@gmail.com>
2022-11-13 00:11:59 +0200

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  safelist: [
+    {
+      pattern: /./,
+      variants: ["md", "lg", "xl"], // you can add your variants here
+    },
+  ],
   content: ["./public/**/*.html"],
   theme: {
     container: false,
