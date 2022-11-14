b95d188826d5fd1493deb0b2409211c2c3ea000a
Clinton <manakanejustin@gmail.com>
2022-11-14 19:01:58 +0200

    made updates to the sliders

--- public/app.js
+++ public/app.js
@@ -1,9 +1,29 @@
 /* Nav */
-const comingSoonBtn = document.querySelector("[data-coming-soon]");
+const menuBtn = document.querySelector("[data-nav-button]");
+const navLinks = document.querySelector("[data-nav-links]");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  if (navLinks.classList.contains("active")) {
+    document.querySelector(".hamburger").classList.add("hidden");
+    document.querySelector(".cross").classList.remove("hidden");
+  } else {
+    document.querySelector(".hamburger").classList.remove("hidden");
+    document.querySelector(".cross").classList.add("hidden");
+  }
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  document.querySelector(".hamburger").classList.remove("hidden");
+  document.querySelector(".cross").classList.add("hidden");
+});
+
+/* Emailing list */
+const comingSoonBtn = document.querySelector("[data-coming-button]");
 const emailInput = document.querySelector("[data-email-input]");
 
 comingSoonBtn.addEventListener("click", () => {
-  console.log("Heyyyyy");
   emailInput.focus();
 });
 
@@ -31,49 +51,29 @@ const swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   slidesOffsetAfter: 0,
 
-  /*  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  }, */
-
   // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-
-  // And if we need scrollbar
-  /* scrollbar: {
-    el: ".swiper-scrollbar",
-  }, */
 });
 
-/* const slidesContainer = document.querySelector(".slides-container");
-const slides = document.querySelectorAll(".slide-item");
+/* Slider:About us */
+var myswiper = new Swiper(".mySwiper", {
+  // Optional parameters
+  speed: 2500,
+  direction: "horizontal",
+  loop: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  lazy: true,
+  slidesPerView: 1,
+  slidesOffsetAfter: 0,
 
-setInterval(() => {
-  let flag = false;
-  let i = 0;
-  while (!flag) {
-    if (i === slides.length - 1) {
-      if (slides[i].classList.contains("block")) {
-        slides[i].classList.remove("block");
-        slides[i].classList.add("hidden");
-
-        slides[0].classList.remove("hidden");
-        slides[0].classList.add("block");
-        flag = true;
-      }
-    } else {
-      if (slides[i].classList.contains("block")) {
-        slides[i].classList.remove("block");
-        slides[i].classList.add("hidden");
-        slides[i + 1].classList.remove("hidden");
-        slides[i + 1].classList.add("block");
-        flag = true;
-      }
-    }
-    i++;
-  }
-}, 8000);
- */
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  safelist: [
-    {
-      pattern: /./,
-      variants: ["md", "lg", "xl"], // you can add your variants here
-    },
-  ],
   content: ["./public/**/*.html"],
   theme: {
     container: false,
